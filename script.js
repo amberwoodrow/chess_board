27a8056d629353d32adf65e9b7c6201234b1f4df
amberwoodrow <amberwoodrow@gmail.com>
2015-07-11 23:11:00 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// The instructions asked to make a chess board that takes any number and makes a
+// grid. I would either make a neat looking grid that looks like
+// a checkerboard so I did size by size using '# ' or ' #'. I did not do it counting
+// each space and # separate.
+var size = prompt("Pick a number to create the chess board size you want.");
+// store the rows and cols to print out later
+var chessBoard = "";
+// makes rows
+for(var row=0; row<size; row++) {
+  // makes columns
+  for(var col=0; col<size; col++) {
+    // if the row is even store this
+    if (row % 2 === 0) {
+      chessBoard += '# ';
+      // else store this for odd rows
+    }else {
+      chessBoard += ' #';
+    }
+    // when the column gets to the end of the row add a new line.
+    // col+1 keeps the product from being 0 first and adding # at the end and front.
+    if((col+1) % size === 0) {
+        chessBoard += "\n";
+    }
+  }
+}
+console.log(chessBoard);
